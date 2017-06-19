3bfe90001b953d1e59aaef3285c26d9b45041396
Lucas Weyne Barros Ferreira <lucasferreira@great.local.ufc.br>
2017-06-19 11:10:02 -0300

    Change InvalidEClassError messages

--- src/main/groovy/gorm/emf/consts/Messages.groovy
+++ src/main/groovy/gorm/emf/consts/Messages.groovy
@@ -1,6 +1,7 @@
 package gorm.emf.consts
 
 import org.eclipse.emf.ecore.EClass
+import org.eclipse.emf.ecore.EPackage
 
 /**
  * Created by lucasferreira on 14/06/2017.
@@ -8,7 +9,7 @@ import org.eclipse.emf.ecore.EClass
 class Messages {
 
     /**
-     * Error/Exception messages
+     * Exception messages
      */
-    static final String INVALID_ECLASS_FORMAT = "Classe '%1s' não encontrada para o EPackage '%2s' ou não é uma instância de ${EClass.class.simpleName}"
+    static final String INVALID_ECLASS_FORMAT = "Class '%1s' not found for '${EPackage.class.simpleName}' '%2s' or is not an instance of '${EClass.class.simpleName}'"
 }
